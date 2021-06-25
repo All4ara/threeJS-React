ef7e024e7caff017ce3a17b5d50a94d34d0ee27f
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-06-24 20:07:05 -0400

    error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from "react";
-import { Canvas } from "react-three-fiber";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
-
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 import "./App.css";
 
 function Box() {
-  
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
   return (
-    <mesh>
+    <mesh
+      onClick={() => {
+        api.velocity.set(0, 2, 0);
+      }}
+      ref={ref}
+      position={[0, 2, 0]}
+    >
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
   );
 }
 
-
+function Plane() {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+  return (
+    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
 
 export default function App() {
   return (
@@ -23,9 +39,10 @@ export default function App() {
       <Stars />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
-      
-      <Box />
-        
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
     </Canvas>
   );
 }
